13f76ab6a3525a0081e26113dc5f8f22a745312e
Mhycka1 <mmcdon10@terpmail.umd.edu>
2024-01-20 14:14:38 -0500

    added calculation of carbon footprint

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
@@ -10,8 +10,27 @@ def home():
 def sign_up():
     return render_template("sign-up.html")
 
-@views.route('/quiz')
+@views.route('/quiz', methods=['GET', 'POST'])
 def quiz():
+    if request.method == 'POST':
+        electric_bill = request.form.get('electric-bill') * 105
+        gas_bill = request.form.get('gas-bill') * 105
+        oil_bill = request.form.get('oil-bill') * 113
+        car_mileage = request.form.get('car-mileage') * .79
+        sub_4_flights = request.form.get('flights-less-than-4-hours') * 1100
+        over_4_flights = request.form.get('flights-more-than-4-hours') * 4,400
+        recycle_newspaper = request.form.get('recycle-newspaper')
+        recycle_metal = request.form.get('recycle-aluminum-tin')
+
+        footprint = electric_bill + gas_bill + oil_bill + car_mileage + sub_4_flights + over_4_flights
+
+        if recycle_newspaper == 'n':
+            footprint = footprint + 184
+        
+        if recycle_metal == 'n':
+            footprint = footprint + 166
+
+
     return render_template("quiz.html")
 
 @views.route('/main_dashboard')
